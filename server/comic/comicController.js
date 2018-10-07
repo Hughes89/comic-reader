c32ab4804dfee008382a2cfc2ceaaece2ad98dec
Hughes89 <mik3h89@gmail.com>
2018-10-07 00:08:31 -0400

    added comments

--- server/comic/comicController.js
+++ server/comic/comicController.js
@@ -1,24 +1,73 @@
+/*
+ * @CreateTime: Oct 6, 2018 11:57 PM
+ * @Author: Mike Hughes
+ * @Contact: mik3h89@gmail.com
+ * @Last Modified By: Mike Hughes
+ * @Last Modified Time: Oct 7, 2018 12:03 AM
+ * @Description: Controller for comic routes
+ */
+
+// =============================================================================
+// Requires
+// =============================================================================
+
 const path = require('path');
 const fs = require('fs');
 
+
+// =============================================================================
+// DUMMY DATA // TODO Remove
+// =============================================================================
 let dummyDatabase = {
     1: 'The Flash 047 (2018) (2 covers) (digital) (Minutemen-Thoth)'
 };
 
-module.exports = {
-    
-    getAllPages: (req, res) => {
-        let comicId = req.params.id;
-        let comicPath = path.join(__dirname, '/../', `${dummyDatabase[comicId]}`);
-        let comicPages = fs.readdirSync(comicPath).map(file => file);
-        res.json(comicPages);
-    },
-    
-    getPage: (req, res) => {
-        let id = req.params.id;
-        let filename = req.params.filename;
-        let pagePath = path.join(__dirname, '/../', `${dummyDatabase[id]}/${filename}`)
-        res.sendFile(pagePath);
-    }
+// =============================================================================
+// Functions
+// =============================================================================
 
-};
+const getAllPages = (req, res) => {
+    let comicId = req.params.id;
+    let comicPath = path.join(__dirname, '/../', `${dummyDatabase[comicId]}`);
+    let comicPages = fs.readdirSync(comicPath).map(file => file);
+    res.json(comicPages); 
+}
+
+const getPage = (req, res) => {
+    let id = req.params.id;
+    let filename = req.params.filename;
+    let pagePath = path.join(__dirname, '/../', `${dummyDatabase[id]}/${filename}`)
+    res.sendFile(pagePath);
+}
+
+
+// =============================================================================
+// Export
+// =============================================================================
+
+module.exports = {
+    getAllPages,
+    getPage
+}
+
+
+// module.exports = {
+//     /**
+//      * @param  {} req
+//      * @param  {} res
+//      */
+//     getAllPages: (req, res) => {
+//         let comicId = req.params.id;
+//         let comicPath = path.join(__dirname, '/../', `${dummyDatabase[comicId]}`);
+//         let comicPages = fs.readdirSync(comicPath).map(file => file);
+//         res.json(comicPages);
+//     },
+    
+//     getPage: (req, res) => {
+//         let id = req.params.id;
+//         let filename = req.params.filename;
+//         let pagePath = path.join(__dirname, '/../', `${dummyDatabase[id]}/${filename}`)
+//         res.sendFile(pagePath);
+//     }
+
+// };
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,30 @@
+/*
+ * @CreateTime: Oct 6, 2018 11:38 PM
+ * @Author: Mike Hughes
+ * @Contact: mik3h89@gmail.com
+ * @Last Modified By: Mike Hughes
+ * @Last Modified Time: Oct 7, 2018 12:08 AM
+ * @Description: Handle all routing used by the comic server
+ */
+
+
+// =============================================================================
+// Express - Router
+// =============================================================================
 const express = require('express');
+const router = express.Router();
+
+
+// =============================================================================
+// Controllers
+// =============================================================================
 
 const comicController = require('../comic/comicController');
 
-const router = express.Router();
+
+// ========================================================================
+// Comic - Routes (/comic/)
+// ========================================================================
 
 router.route('/comic/:id/pages').get(comicController.getAllPages);
 router.route('/comic/:id/:filename').get(comicController.getPage);
